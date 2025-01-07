c4b4c683309bfa4aa35c1edf45d26a73be778be6
Ramiz Balayil <ramizbalayil@gmail.com>
2025-01-07 08:26:00 +0530

    [added] lerp for health bar

--- Assets/Scripts/UI/UIHealthBar.cs
+++ Assets/Scripts/UI/UIHealthBar.cs
@@ -10,22 +10,34 @@ namespace towerdefence.ui
         [SerializeField] private Color _HealthColorWarning;
         [SerializeField] private Color _HealthColorDanger;
 
-
+        private float mStartFillAmount = 1f;
+        private float mEndFillAmount = 1f;
+        private float mElapsedTime = 0f;
+        private float mDesiredDuration = 0.5f;
         public void UpdateUIHealth(float healthPerc)
         {
             healthPerc = Mathf.Max(0f, healthPerc);
-            _HealthBar.fillAmount = healthPerc;
-            UpdateUIHealthColor(healthPerc);
+            mStartFillAmount = _HealthBar.fillAmount;
+            mEndFillAmount = healthPerc;
+            mElapsedTime = 0f;
         }
 
-        private void UpdateUIHealthColor(float healthPerc)
+        private void UpdateUIHealthColor()
         {
+            float healthPerc = _HealthBar.fillAmount;
             if (healthPerc > 0.5f)
                 _HealthBar.color = _HealthColorNormal;
             else if (healthPerc > 0.25)
                 _HealthBar.color = _HealthColorWarning;
             else
                 _HealthBar.color = _HealthColorDanger;
+        }
+
+        private void Update()
+        {
+            mElapsedTime += Time.deltaTime;
+            _HealthBar.fillAmount = Mathf.Lerp(mStartFillAmount, mEndFillAmount, mElapsedTime / mDesiredDuration);
+            UpdateUIHealthColor();
         }
     }
 }
