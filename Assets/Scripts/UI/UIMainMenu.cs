8f06a4e3dad7b3d1151537434c07e6584c30627e
Ramiz Balayil <ramizbalayil@gmail.com>
2025-01-09 23:11:54 +0530

    [added] start and end text on levels, updated text in drag and drop ui, Application exit feature

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -1,5 +1,6 @@
 using frameworks.services;
 using frameworks.ui;
+using System;
 using towerdefence.services;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,6 +13,7 @@ namespace towerdefence.ui
 
         [SerializeField] private Button _PlayGameButton;
         [SerializeField] private Button _UpgradeHeroesButton;
+        [SerializeField] private Button _ExitGameButton;
         [SerializeField] private GameObject _UpgradeNotification;
 
         protected override void Awake()
@@ -19,12 +21,19 @@ namespace towerdefence.ui
             base.Awake();
             _PlayGameButton.onClick.AddListener(OnPlayGamesButtonClicked);
             _UpgradeHeroesButton.onClick.AddListener(OnUpgradeHeroesButtonClicked);
+            _ExitGameButton.onClick.AddListener(OnExitGameButtonPressed);
         }
 
         private void OnDestroy()
         {
             _PlayGameButton.onClick.RemoveListener(OnPlayGamesButtonClicked);
             _UpgradeHeroesButton.onClick.RemoveListener(OnUpgradeHeroesButtonClicked);
+            _ExitGameButton.onClick.RemoveListener(OnExitGameButtonPressed);
+        }
+
+        private void OnExitGameButtonPressed()
+        {
+            Application.Quit();
         }
 
         public override void Show()
